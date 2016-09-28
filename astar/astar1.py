3b2a1fe019d2ff91173ecd9a550da528ba1c85f0
Christina Hellenes Andresen <christina.h.andresen@gmail.com>
2016-09-28 18:22:31 +0200

    Can find current nodes and neigthbours, started with heuristic

--- astar/astar1.py
+++ astar/astar1.py
@@ -1,7 +1,66 @@
+try:
+    import Queue as Q  # ver. < 3.0
+except ImportError:
+    import queue as Q
+
+
+def heuristic(node, end_node):
+    return abs(node.x - end_node.x) + (node.y - end_node.y)
+
 
 def run_astar(board):
     if not isinstance(board, list):
         return "Board must be a list"
+
+    priority_queue = Q.PriorityQueue()
+    start_node = ""
+    end_node = ""
+    board_width = len(board[0])
+    board_height = len(board)
+
+
+    for i in board:
+        for j in i:
+            if j.start:
+                start_node = j
+            if j.end:
+                end_node = j
+
+    priority_queue.put(start_node, 0)
+
+    node_from = {}
+    total_cost = {}
+    node_from[start_node] = None
+    total_cost[start_node] = 0
+
+    while not priority_queue.empty():
+        current_node = priority_queue.get()
+
+        if current_node == end_node:
+            break
+
+        get_adjacent_nodes(current_node, board_width, board_height)
+
+
+def get_adjacent_nodes(current_node, board_w, board_h):
+    nodes = []
+    print(current_node, board_w, board_h)
+    if current_node.x > 0:
+        nodes.append(current_node.x - 1)
+
+    if current_node.y > 0:
+        nodes.append(current_node.y - 1)
+
+    if current_node.x < board_h:
+        nodes.append(current_node.x + 1)
+
+    print(current_node.y)
+    print(board_h)
+    if current_node.y < board_w:
+        nodes.append(current_node.y + 1)
+
+    print(nodes)
+
 
 
 class Node(object):
@@ -15,8 +74,8 @@ class Node(object):
         self.walkable = character != "#"
 
     def __str__(self):
-        #return 'Node(%i, %i, %s): %s' % (self.x, self.y, self.character, self.walkable)
-        return self.character
+        return 'Node(%i, %i, %s): %s' % (self.x, self.y, self.character, self.walkable)
+        #return self.character
 
     def __repr__(self):
         return self.__str__()
@@ -38,6 +97,10 @@ def read_board(board_id='1-1'):
 
 if __name__ == "__main__":
     board = read_board(board_id="1-1")
-    print(board)
+
+    start = ""
+    end = ""
+
+    #print(board)
 
     run_astar(board)
